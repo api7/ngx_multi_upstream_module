f72f26adc317d60b7af601ea11e1a580291a540c
spacewander <spacewanderlzx@gmail.com>
2021-12-07 19:18:26 +0800

    feat: support Nginx 1.19.9

--- patch.sh
+++ patch.sh
@@ -19,13 +19,17 @@ if [[ $# != 1 ]]; then
     usage "$0"
 fi
 
-if [[ $1 != *openresty-1.19.3.* ]]; then
+if [[ "$1" == *openresty-1.19.3.* ]]; then
+    patch="$PWD/nginx-1.19.3.patch"
+    dir="$1/bundle/nginx-1.19.3"
+elif [[ "$1" == *openresty-1.19.9.* ]]; then
+    patch="$PWD/nginx-1.19.9.patch"
+    dir="$1/bundle/nginx-1.19.9"
+else
     err "can't detect OpenResty version"
     exit 1
 fi
 
-patch="$PWD/nginx-1.19.3.patch"
-dir="$1/bundle/nginx-1.19.3"
 cd "$dir" || failed_to_cd "$dir"
 echo "Start to patch $patch to $dir..."
 patch -p0 --verbose < "$patch"
