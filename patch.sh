e6324564936c0c17cfc551e13bc40a4a33aa0f07
Shreemaan Abhishek <shreemaan.abhishek@gmail.com>
2025-02-06 16:55:40 +0545

    chore: prepare to release openresty v1.27.1.1 (#17)

--- patch.sh
+++ patch.sh
@@ -31,6 +31,9 @@ elif [[ "$1" == *openresty-1.21.4.* ]]; then
 elif [[ "$1" == *openresty-1.25.3.* ]]; then
     patch="$PWD/nginx-1.25.3.patch"
     dir="$1/bundle/nginx-1.25.3"
+elif [[ "$1" == *openresty-1.27.1.* ]]; then
+    patch="$PWD/nginx-1.27.1.patch"
+    dir="$1/bundle/nginx-1.27.1"
 else
     err "can't detect OpenResty version"
     exit 1
